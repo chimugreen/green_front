d584d240282eab3b14a4b64ec199bbcaaa08a302
Chan <vicg0923@gmail.com>
2025-11-27 16:55:25 +0900

    feat: dm창 ui만

--- src/routers/ChatPage.tsx
+++ src/routers/ChatPage.tsx
@@ -3,7 +3,41 @@
 const ChatPage = () => {
   return (
     <>
-      <div>알림페이지</div>
+      <div>DM</div>
+      <div className="flex items-center justify-around">
+        <div
+          className="flex flex-col items-center justify-start
+         p-3 w-200 min-w-2 h-180 border-2 rounded-2xl bg-gray-200"
+        >
+          <div className="flex justify-start flex-row">
+            <p className="flex m-3 p-2 border-2 rounded-2xl flex-row bg-white">
+              profile
+            </p>
+            <p className="flex m-3 p-2 border-2 rounded-2xl flex-row bg-white">
+              profile
+            </p>
+            <p className="flex m-3 p-2 border-2 rounded-2xl flex-row bg-white">
+              profile
+            </p>
+            <p className="flex m-3 p-2 border-2 rounded-2xl flex-row bg-white">
+              profile
+            </p>
+          </div>
+          <div className="border-2 rounded-2xl m-2 p-2 bg-white w-full h-full">
+            <p>name</p>
+            <hr />
+          </div>
+          <div className="flex gap-2 w-full">
+            <input
+              className="w-full h-auto border-2 rounded-2xl bg-white w-"
+              type="text"
+            />
+            <button className="m-1 p-1 border-2 rounded-2xl bg-white">
+              발송
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
